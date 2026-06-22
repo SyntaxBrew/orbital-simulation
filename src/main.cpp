a70afa3235e369979c6ea6c320ca3cda0fa70a51
SyntaxBrew <SyntaxBrew@outlook.com>
2026-06-22 13:38:00 -0700

    Add basic celestial body update loop for velocity + position, and drawing loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,36 @@
 #include "raylib.h"
+#include "raymath.h"
+
 #include <iostream>
+#include <vector>
 
 #include "body.hpp"
 
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 800;
+
 int main() {
-    InitWindow(800, 800, "Orbital Simulation");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Orbital Simulation");
     SetTargetFPS(60);
+
+    std::vector<Body> staticBodies{};
+    std::vector<Body> dynamicBodies{};
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
+
+        float deltaTime = GetFrameTime();
+
+        for (Body& body: dynamicBodies) {
+            body.velocity = Vector2Add(body.velocity, Vector2Scale(body.acceleration, deltaTime));
+            body.position = Vector2Add(body.position, Vector2Scale(body.velocity, deltaTime));
+        }
+
+        for (Body& body: dynamicBodies) {
+            DrawCircle(body.position.x, body.position.y, 16, GREEN);
+        }
+
         EndDrawing();
     }
 
