88bd6d6212c8c20d94bd70f9e00cca6b8609a344
SyntaxBrew <SyntaxBrew@outlook.com>
2026-06-22 15:22:35 -0700

    Add force visualization arrows, and better calculations for net acceleration

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,14 @@ const int WINDOW_HEIGHT = 800;
 
 const float GRAV_CONST = 1000;
 
+const float ARROW_LENGTH = 64;
+const float ARROW_HEAD_LENGTH = 24;
+const float ARROW_HEAD_SIZE = 6;
+
+float vector2_magnitudef(Vector2 vector2) {
+    return sqrtf(vector2.x * vector2.x + vector2.y * vector2.y);
+}
+
 int main() {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Orbital Simulation");
     SetTargetFPS(60);
@@ -41,30 +49,47 @@ int main() {
         float deltaTime = GetFrameTime();
 
         for (Body& dynamic_body: dynamic_bodies) {
+            Vector2 total_acceleration{0, 0};
+
             for (Body& static_body: static_bodies) {
                 float dx = dynamic_body.position.x - static_body.position.x;
                 float dy = dynamic_body.position.y - static_body.position.y;
-
-                std::cout << "(" << sqrtf(dx * dx + dy * dy) << ")\n";
+                float distance = sqrtf(dx * dx + dy * dy);
     
                 float angle = atan2f(dy, dx);
-                float grav_force_magnitude = GRAV_CONST * (dynamic_body.mass * static_body.mass) / (dx * dx + dy * dy);
+                float grav_force_magnitude = GRAV_CONST * (dynamic_body.mass * static_body.mass) / (distance * distance);
 
-                Vector2 grav_force = Vector2{-grav_force_magnitude * cosf(angle), -grav_force_magnitude * sinf(angle)};
-                dynamic_body.acceleration = Vector2Scale(grav_force, 1 / dynamic_body.mass);
-
-                DrawLineEx(dynamic_body.position, static_body.position, 4.0f, BLUE);
+                Vector2 grav_force{-grav_force_magnitude * cosf(angle), -grav_force_magnitude * sinf(angle)};
+                total_acceleration = Vector2Add(total_acceleration, Vector2Scale(grav_force, 1 / dynamic_body.mass));
             } 
 
+            dynamic_body.acceleration = total_acceleration;
             dynamic_body.velocity = Vector2Add(dynamic_body.velocity, Vector2Scale(dynamic_body.acceleration, deltaTime));
             dynamic_body.position = Vector2Add(dynamic_body.position, Vector2Scale(dynamic_body.velocity, deltaTime));
         }
 
+           
         for (Body& body: static_bodies) {
             DrawCircle(body.position.x, body.position.y, 32, RED);
         }
+      
         for (Body& body: dynamic_bodies) {
             DrawCircle(body.position.x, body.position.y, 16, GREEN);
+
+            float magnitude = vector2_magnitudef(body.acceleration);
+            Vector2 force_dir{body.acceleration.x / magnitude, body.acceleration.y / magnitude};
+            Vector2 end_position = Vector2Add(body.position, Vector2Scale(force_dir, ARROW_LENGTH));
+            DrawLineEx(body.position, end_position, 4.0f, BLUE);
+
+            Vector2 perp_dir{-force_dir.y, force_dir.x};
+            Vector2 head_position{Vector2Add(end_position, Vector2Scale(force_dir, ARROW_HEAD_LENGTH))};
+
+            DrawTriangle(
+                head_position,
+                Vector2{ end_position.x - perp_dir.x * ARROW_HEAD_SIZE, end_position.y - perp_dir.y * ARROW_HEAD_SIZE },
+                Vector2{ end_position.x + perp_dir.x * ARROW_HEAD_SIZE, end_position.y + perp_dir.y * ARROW_HEAD_SIZE },
+                BLUE
+            );
         }
 
         EndDrawing();
