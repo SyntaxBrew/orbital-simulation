413b1c29701511c6b2b6320c858ee02469eaede9
SyntaxBrew <SyntaxBrew@outlook.com>
2026-06-22 17:48:07 -0700

    Add trail effect for dynamic bodies, add option for disabling force arrows

--- include/body.hpp
+++ include/body.hpp
@@ -1,10 +1,13 @@
 #include "raylib.h"
+#include <deque>
 
 struct Body {
     float mass;
     Vector2 position;
     Vector2 velocity;
     Vector2 acceleration;
+
+    std::deque<Vector2> prev_positions;
 
     Body(float mass, Vector2 initialPosition, Vector2 initialVelocity);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,12 @@ const int WINDOW_HEIGHT = 800;
 
 const float GRAV_CONST = 1000;
 
+const bool ARROWS_ENABLED = false;
 const float ARROW_LENGTH = 64;
 const float ARROW_HEAD_LENGTH = 24;
 const float ARROW_HEAD_SIZE = 6;
+
+const int MAX_TRAIL_SIZE = 128;
 
 float vector2_magnitudef(Vector2 vector2) {
     return sqrtf(vector2.x * vector2.x + vector2.y * vector2.y);
@@ -68,6 +71,11 @@ int main() {
             dynamic_body.acceleration = total_acceleration;
             dynamic_body.velocity = Vector2Add(dynamic_body.velocity, Vector2Scale(dynamic_body.acceleration, deltaTime));
             dynamic_body.position = Vector2Add(dynamic_body.position, Vector2Scale(dynamic_body.velocity, deltaTime));
+
+            if (dynamic_body.prev_positions.size() >= MAX_TRAIL_SIZE) {
+                dynamic_body.prev_positions.pop_front();
+            }
+            dynamic_body.prev_positions.push_back(dynamic_body.position);
         }
 
         BeginDrawing();
@@ -78,22 +86,34 @@ int main() {
         }
       
         for (Body& body: dynamic_bodies) {
+
+            if (ARROWS_ENABLED) {
+                float magnitude = vector2_magnitudef(body.acceleration);
+                Vector2 force_dir{body.acceleration.x / magnitude, body.acceleration.y / magnitude};
+                Vector2 end_position = Vector2Add(body.position, Vector2Scale(force_dir, ARROW_LENGTH));
+                DrawLineEx(body.position, end_position, 4.0f, BLUE);
+
+                Vector2 perp_dir{-force_dir.y, force_dir.x};
+                Vector2 head_position{Vector2Add(end_position, Vector2Scale(force_dir, ARROW_HEAD_LENGTH))};
+            
+                DrawTriangle(
+                    head_position,
+                    Vector2{ end_position.x - perp_dir.x * ARROW_HEAD_SIZE, end_position.y - perp_dir.y * ARROW_HEAD_SIZE },
+                    Vector2{ end_position.x + perp_dir.x * ARROW_HEAD_SIZE, end_position.y + perp_dir.y * ARROW_HEAD_SIZE },
+                    BLUE
+                );
+            }
+            
+
+            for (int i = 1; i < body.prev_positions.size(); i++) {
+                Vector2 start_pos = body.prev_positions.at(i-1);
+                Vector2 end_pos = body.prev_positions.at(i);
+
+                float alpha = (float) i / body.prev_positions.size();
+                DrawLineEx(start_pos, end_pos, 2.0f, Fade(BLUE, alpha));
+            }
+            
             DrawCircle(body.position.x, body.position.y, 16, GREEN);
-
-            float magnitude = vector2_magnitudef(body.acceleration);
-            Vector2 force_dir{body.acceleration.x / magnitude, body.acceleration.y / magnitude};
-            Vector2 end_position = Vector2Add(body.position, Vector2Scale(force_dir, ARROW_LENGTH));
-            DrawLineEx(body.position, end_position, 4.0f, BLUE);
-
-            Vector2 perp_dir{-force_dir.y, force_dir.x};
-            Vector2 head_position{Vector2Add(end_position, Vector2Scale(force_dir, ARROW_HEAD_LENGTH))};
-
-            DrawTriangle(
-                head_position,
-                Vector2{ end_position.x - perp_dir.x * ARROW_HEAD_SIZE, end_position.y - perp_dir.y * ARROW_HEAD_SIZE },
-                Vector2{ end_position.x + perp_dir.x * ARROW_HEAD_SIZE, end_position.y + perp_dir.y * ARROW_HEAD_SIZE },
-                BLUE
-            );
         }
 
         EndDrawing();
